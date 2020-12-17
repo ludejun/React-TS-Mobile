f447ed36caaba6daa93f51c1e591a6305b65d11f
ludejun <david.lu@loyalvalleycapital.com>
2020-12-17 18:52:44 +0800

    更改路由组件使用react-keeper，更新readme

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 ## 移动端React脚手架项目
 
 使用技术栈：
-React + React-Router + webpack + typescript + mock + eslint + prettier + fetch
+React + React-Keeper + webpack + typescript + mock + eslint + prettier + fetch
 
 在PC端的基础上去除了Redux和Rematch，移动端对公共状态管理的需求不强烈，对加载性能要求高。并可以适当在webpack的entry中加上JS代码拆分。
+
+**生产打包：cached包是不太会变的包，做长期缓存或者放到APP本地，react、react-dom、@babel/polyfill 不要随意升级**
+
+兼容本地Mock和Mock Server两种mock方式。本地Mock使用根目录的mock文件夹，按照示例编写即可。MockServer通过src/config/index进行MockServer URL和白名单控制，通过yarn start:mock来进行本地Mock开发调试。
+
+路由使用React-Keeper，添加路由跳转缓存的方法，在后退或者跳转到缓存页时使用之前的页面状态。更多方法介绍：https://github.com/lanistor/react-keeper
 
 #### 项目命令行
 
@@ -24,15 +30,14 @@ React + React-Router + webpack + typescript + mock + eslint + prettier + fetch
 
 #### 特性
 
-1. 最新的react框架，自由使用class或者hooks；添加最成熟的redux作为状态管理、router作为路由管理工具；
-2. 使用[rematch]( https://github.com/rematch/rematch)作为redux中间件，尽量减少模版代码，极大提高开发效率；
-3. 使用最新webpack作为打包，区分开发/测试/内测/生产环境；集成热加载，提高开发测试效率；
-4. 开发/生产环境集成包体积命令行，准确了解各包体积大小及组成，杜绝大依赖包引入（如禁止lodash、moment引入生产）；
-5. 静态资源分常变更和不常变更类型，变更资源name上添加hash，自动更新html引入；
-6. 使用eslint+prettier组合规范前端代码，统一各项目代码格式，自动修改代码到规范配置；
-7. 将代码规范强制绑定git commit，提交代码前自动校验并修复代码规范，不规范代码不能提交；
-8. 使用mockjs作为前端本地API mock工具，自动在开发模式下，无代码侵入代理前端代码发出的XMLHttpRequest请求;
-9. 集成typescript，提高前端代码质量，在编译模式发现更多可能bug；
+1. 最新的react框架，自由使用class或者hooks；
+2. 使用最新webpack作为打包，区分开发/测试/内测/生产环境；集成热加载，提高开发测试效率；
+3. 开发/生产环境集成包体积命令行，准确了解各包体积大小及组成，杜绝大依赖包引入（如禁止lodash、moment引入生产）；
+4. 静态资源分常变更和不常变更类型，变更资源name上添加hash，自动更新html引入；
+5. 使用eslint+prettier组合规范前端代码，统一各项目代码格式，自动修改代码到规范配置；
+6. 将代码规范强制绑定git commit，提交代码前自动校验并修复代码规范，不规范代码不能提交；
+7. 使用mockjs作为前端本地API mock工具，自动在开发模式下，无代码侵入代理前端代码发出的XMLHttpRequest请求;
+8. 集成typescript，提高前端代码质量，在编译模式发现更多可能bug；
 10. 使用fetch作为ajax库，api各环节高度配置化；
 11. 丰富的路由、组件、同步action、异步action、reducer、请求处理、基础函数等示例，可以直接参考上手开发。
 
@@ -41,6 +46,8 @@ React + React-Router + webpack + typescript + mock + eslint + prettier + fetch
 
 
 #### 参考文档
+
+[react-keeper使用](https://github.com/lanistor/react-keeper)
 
 [fetch使用](https://github.github.io/fetch/)
 
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+import { CacheLink } from 'react-keeper';
 import Count from './components/Count';
 import logo from './logo.svg';
 import './App.less';
@@ -9,10 +9,66 @@ interface IAppProps {
   increment?: () => void;
   incrementAsync?: () => void;
 }
+declare global {
+  interface Window {
+    WKWebViewJavascriptBridge: any,
+    WKWVJBCallbacks: any,
+    webkit: any
+  }
+}
 
 export default class App extends React.Component<IAppProps, {}> {
   constructor(props: IAppProps) {
     super(props);
+  }
+
+  componentDidMount() {
+    // JSBridge方法统一调用处
+    function setupWKWebViewJavascriptBridge(callback: any) {
+      if (window.WKWebViewJavascriptBridge) { return callback(window.WKWebViewJavascriptBridge); }
+      if (window.WKWVJBCallbacks) { return window.WKWVJBCallbacks.push(callback); }
+      window.WKWVJBCallbacks = [callback];
+      if (window.webkit) {
+        window.webkit.messageHandlers.iOS_Native_InjectJavascript.postMessage(null);
+      }
+    }
+
+    setupWKWebViewJavascriptBridge(function (bridge: any) {
+      var uniqueId = 1;
+      function log(message: string, data?: any, type?: string) {
+        var log = document.getElementById('log');
+        var el = document.createElement('div');
+        el.className = type == 'native' ? 'logLine_Native' : 'logLine_JS';
+        el.innerHTML = uniqueId++ + '. ' + message + ':<br/>' + JSON.stringify(data);
+        if (log) {
+          if (log.children.length) {
+            log.insertBefore(el, log.children[0])
+          } else {
+            log.appendChild(el)
+          }
+        }
+      }
+      //APP调用JS
+      bridge.registerHandler('ZXGJavascriptHandler', function (data: any, responseCallback: any) {
+        log('iOS called testJavascriptHandler with', data, 'native')
+        var responseData = { 'Javascript Says': 'Right back atcha!' }
+        log('JS responding with', responseData, 'native')
+        responseCallback(responseData)
+      })
+
+      document.body.appendChild(document.createElement('br'))
+
+      var callbackButton = (document.getElementById('buttons') as Element).appendChild(document.createElement('button'))
+      callbackButton.innerHTML = 'js调用原生APP';
+      //JS调用APP
+      callbackButton.onclick = function (e) {
+        e.preventDefault();
+        log('JS calling handler "testiOSCallback"');
+        bridge.callHandler('ZXGNativeJavascriptHandler', { 'foo': 'bar' }, function (response: any) {
+          log('JS got response', response, 'js');
+        });
+      }
+    })
   }
 
   render() {
@@ -33,11 +89,11 @@ export default class App extends React.Component<IAppProps, {}> {
             Learn React
           </a>
           <p>计数中：{this.props.count}</p>
-          {/* <Count onAddClick={increment} onAddAsyncClick={incrementAsync} /> */}
+          <div id='buttons'></div>
+          <div id='log'></div>
 
-          <Link to="/login" style={{ marginTop: 20 }}>
-            到登陆页
-          </Link>
+          {/* <Count onAddClick={increment} onAddAsyncClick={incrementAsync} /> */}
+          <CacheLink to="/404"></CacheLink>
         </header>
       </div>
     );
